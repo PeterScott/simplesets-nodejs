2d545be4954dc74041af42bcfa7883a4d17023e9
Peter Scott <pjscott@iastate.edu>
2010-09-19 19:01:46 -0500

    Added test for add() and remove() chaining, and cleaned up syntax.

--- test/testsets.js
+++ test/testsets.js
@@ -28,10 +28,10 @@ assert.ok(!s1.equals(s2));
 assert.ok(typeof s1.array() == typeof [1, 2, 3]);
 
 // Test two-set operations, using mixed number and string keys.
-s3 = new sets.Set([1, 2, 3, "1", "2", "3"]);
-s4 = new sets.Set([3, "1", "foo"]);
-nullset = new sets.Set();
-s3sub = new sets.Set([1, "3", 2]);
+var s3 = new sets.Set([1, 2, 3, "1", "2", "3"]);
+var s4 = new sets.Set([3, "1", "foo"]);
+var nullset = new sets.Set();
+var s3sub = new sets.Set([1, "3", 2]);
 assert.ok(s3.issuperset(s3sub));
 assert.ok(!s3sub.issuperset(s3));
 assert.ok(s3sub.issubset(s3));
@@ -67,6 +67,11 @@ assert.ok(sb.issubset(sa));
 assert.ok(nullset.pick() === null);
 assert.ok(nullset.pop() === null);
 assert.ok(nullset.issubset(nullset));
+
+// Make sure add and remove return the set, for chaining.
+var s5 = new sets.Set();
+s5.add(3).add(4).remove(3);
+assert.ok(s5.equals(new sets.Set([4])));
 
 // If we got to here, then...
 console.log('All tests passed!');
